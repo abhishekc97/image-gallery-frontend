c3b60732e56202a4d67fbf562ba8658d5c8535ab
Abhishek Choudhari <abhishekc97@gmail.com>
2022-12-18 20:32:21 +0530

    added category api, added fetch by category api

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,23 +1,41 @@
 import React, { useState } from "react";
+import { createSearchParams, useNavigate } from "react-router-dom";
 import styles from "./Filter.module.css";
 
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-
 function Filter() {
-    const [dropdownList, setDropdownList] = useState([]);
-    
+    const navigate = useNavigate();
+
+    function onFilterChange(event) {
+        console.log(event.target.name, event.target.value);
+        if(event.target.value ==="asc" || event.target.value === "desc") {
+            navigate({
+                pathname: "",
+                search:createSearchParams({
+                    sortByDate: event.target.value
+                }).toString()
+            });
+        } else {
+            navigate({
+                pathname: "",
+                search:createSearchParams({
+                    sortByDate: event.target.value
+                }).toString()
+            });
+        }
+    }
+
     return ( 
-        <div className={styles.container}>
-            
-        <DropdownButton id="dropdown-basic-button" title="Filter">
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-        </DropdownButton>
+        <div className={styles.filter}>
+            <select onChange={onFilterChange}>
+                <option value="" selected disabled >Filter</option>
+                <option name="sortByDate" value="asc" >Sort by date (asc)</option>
+                <option name="sortByDate" value="desc" >Sort by date (desc)</option>
+                <option name="filterByLike" value="like">Filter by likes</option>
+            </select>
+        
         
         </div>
-     );
+    );
 }
 
 export default Filter;
