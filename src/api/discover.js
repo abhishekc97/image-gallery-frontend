08b933c6ec4ca25556bf12510ec6a97c129b95fe
Abhishek Choudhari <abhishekc97@gmail.com>
2022-12-23 13:29:17 +0530

    changed backend url for app to access

--- src/api/discover.js
+++ src/api/discover.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // api to fetch all categories
 export async function getCategories() {
-    const reqUrl = "http://127.0.0.1:3000/api/discover/categories"; 
+    // const reqUrl = "http://127.0.0.1:3000/api/discover/categories";
+    const reqUrl = `${process.env.BACKEND_URL}/api/discover/categories`; 
     const results = await axios.get(reqUrl);
     
     if(results.data) {
@@ -14,7 +15,7 @@ export async function getCategories() {
 // api to fetch images based on category and filters
 export async function getGalleryImages(categoryName, sortByDate = "", filterByLikes = "", shuffle = "") {
     
-    const reqUrl = `http://127.0.0.1:3000/api/discover/images/${categoryName}?sortByDate=${sortByDate}&filterByLikes=${filterByLikes}&shuffle=${shuffle}`;
+    const reqUrl = `${process.env.BACKEND_URL}/api/discover/images/${categoryName}?sortByDate=${sortByDate}&filterByLikes=${filterByLikes}&shuffle=${shuffle}`;
     console.log(reqUrl);
     const results = await axios.get(reqUrl);
     
@@ -25,7 +26,7 @@ export async function getGalleryImages(categoryName, sortByDate = "", filterByLi
 
 // add image to favorite api
 export async function likeImage(imageId) {
-    const reqUrl = `http://127.0.0.1:3000/api/discover/like/${imageId}`;
+    const reqUrl = `${process.env.BACKEND_URL}/api/discover/like/${imageId}`;
     console.log(reqUrl);
     const results = await axios.get(reqUrl);
 
